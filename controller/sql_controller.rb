66fb3e80e0a481579a2171409a6844fe8117e2dc
Tech-A34 <tech-a34@Tech-A34s-MacBook-Air.local>
2019-01-08 12:15:04 +0000

    added the route to the score page

--- controller/sql_controller.rb
+++ controller/sql_controller.rb
@@ -1,5 +1,16 @@
 class SqlController < Sinatra::Base 
+    # sets root as the parent-directory of the current file
+    set :root, File.join(File.dirname(__FILE__), '..')
+ 
+    # sets the view directory correctly
+    set :views, Proc.new { File.join(root, "views") }
+
     get '/' do
         "hello world"
     end
+
+    get '/score_page' do
+        erb :score_page
+    end
+
 end
