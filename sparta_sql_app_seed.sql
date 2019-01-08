cf44fbb2facd3b6e7d550d6019e5fcc5f637fc2a
Tech-A06 <hibah28@live.co.uk>
2019-01-08 16:39:19 +0000

    chnaged the data input for students

--- sparta_sql_app_seed.sql
+++ sparta_sql_app_seed.sql
@@ -56,9 +56,7 @@ VALUES ('Engineering-21','2018-11-12', '2018-02-28', 'SDET'),
 INSERT INTO course(CourseName) VALUES ('Business 10'), ('Engineering-01'), ('BIZ 3');
 
 INSERT INTO student (CourseID, FirstName, LastName, Email, Password)
-VALUES
- (01, 'Ian', 'Hawe','ihawe@spartaglobal.com','Acd3my1'),
- (01, 'Hibah', 'Abid','habid@spartaglobal.com','Acd3my1'),
+VALUES (01, 'Hibah', 'Abid','habid@spartaglobal.com','Acd3my1'),
  (01, 'Kirpal', 'Seehra','kseehra@spartaglobal.com','Acd3my1'),
  (01, 'Emem', 'Umoh','eumoh@spartaglobal.com','Acd3my1'),
  (01, 'Thomas', 'Impey','timpey@spartaglobal.com','Acd3my1'),
@@ -71,13 +69,13 @@ VALUES
  (01, 'Arnold', 'Chung','achung@spartaglobal.com','Acd3my1'),
  (02, 'Bob', 'Michaels','bmichaels@spartaglobal.com','Acd3my1'),
  (02, 'Steven', 'Lee','slee@spartaglobal.com','password'),
- (02, 'Jessica', 'Simmo','bmichaels@spartaglobal.com','password47'),
- (05, 'Bob', 'Michaels','bmichaels@spartaglobal.com','hello123'),
- (04, 'Bob', 'Michaels','bmichaels@spartaglobal.com','1234567'),
- (03, 'Bob', 'Michaels','bmichaels@spartaglobal.com','PASSWORD'), 
- (03, 'Bob', 'Michaels','bmichaels@spartaglobal.com','QWErty')
- (03, 'Bob', 'Michaels','bmichaels@spartaglobal.com','PASSWORD123')
- ;
+ (02, 'Jessica', 'Simmon','simmons@spartaglobal.com','password47'),
+ (05, 'Bob', 'Michaels','bmichaels123@spartaglobal.com','hello123'),
+ (04, 'Mollie', 'Impey','mimpey@spartaglobal.com','1234567'),
+ (03, 'Anne', 'Lee','annelee@spartaglobal.com','PASSWORD'), 
+ (03, 'Jeff', 'Goldblum','goldJ@spartaglobal.com','QWErty'),
+ (03, 'Dua', 'Lipa','lipa@spartaglobal.com','PASSWORD123');
+
  INSERT INTO student (CourseID, FirstName, LastName, Email, Password, Score)
  VALUES
  (04, 'Jim', 'Stevens','jsteven@spartaglobal.com','pacman2013', 15),
@@ -85,6 +83,9 @@ VALUES
 
 INSERT INTO trainer (FirstName, LastName, Email, Password)
 VALUES
- ('Dave', 'Collins','trainer1@spartaglobal.com','Admin')
- ('Gary', 'Collins','gary@spartaglobal.com','Admin')
- ('Meryl', 'Streep','MerylStreep@spartaglobal.com','prada');
+ ('Dave', 'Collins','trainer1@spartaglobal.com','Admin'),
+ ('Gary', 'Collins','gary@spartaglobal.com','Admin'),
+ ('Meryl', 'Streep','MerylStreep@spartaglobal.com','prada'),
+ ('Artur', 'Kondas', 'akondas@spartaglobal.com', 'ducks');
+
+INSERT INTO course_trainer (TrainerID, CourseID) VALUES (01, 04), (02,03), (04,01);
