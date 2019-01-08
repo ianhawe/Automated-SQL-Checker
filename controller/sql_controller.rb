dcffa475ca1022625725566c9c09fe7c6a2a4482
Raiyan Shaheen <r.12345@hotmail.co.uk>
2019-01-08 15:23:06 +0000

    added routes to admin search on buttons

--- controller/sql_controller.rb
+++ controller/sql_controller.rb
@@ -1,5 +1,20 @@
 class SqlController < Sinatra::Base 
-    get '/' do
-        "hello world"
-    end
+
+ # sets root as the parent-directory of the current file
+  set :root, File.join(File.dirname(__FILE__), '..')
+
+  # sets the view directory correctly
+  set :views, Proc.new { File.join(root, "views") }
+
+  get '/' do
+      erb  :'pages/index'
+  end
+  
+  get '/admin/login' do
+      erb  :'pages/admin_login'
+  end
+
+  get 'admin/search' do
+      erb  :'pages/search'
+  end
 end
