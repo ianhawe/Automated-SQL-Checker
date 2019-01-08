ca350cea791a5e3bc9c3ab5f755e187932387170
Tech-A42 <tech-a42@Tech-A42s-MacBook-Air.local>
2019-01-08 14:09:07 +0000

    Added content for question three and paths for it

--- controller/sql_controller.rb
+++ controller/sql_controller.rb
@@ -1,5 +1,20 @@
 class SqlController < Sinatra::Base 
-    get '/' do
-        "hello world"
-    end
+    # sets root as the parent-directory of the current file
+  set :root, File.join(File.dirname(__FILE__), '..')
+
+  # sets the view directory correctly
+  set :views, Proc.new { File.join(root, "views") }
+
+ 
+  get '/question_three_page'  do
+    
+    erb :'question_three_page'
+    
+  end
+
+
+
+
+
+
 end
