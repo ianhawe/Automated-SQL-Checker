bb7f5246bd4dd10fe5454ab9afd9202522a0b843
PaulCiobanita <pciobanita@spartaglobal.com>
2019-01-08 15:25:34 +0000

    admin search

--- controller/sql_controller.rb
+++ controller/sql_controller.rb
@@ -1,5 +1,13 @@
 class SqlController < Sinatra::Base 
+
+      # sets root as the parent-directory of the current file
+  set :root, File.join(File.dirname(__FILE__), '..')
+
+  # sets the view directory correctly
+  set :views, Proc.new { File.join(root, "views") }
+
+
     get '/' do
-        "hello world"
+        erb :'pages/admin_search'
     end
 end
