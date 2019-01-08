c40bcb19878e03a6522a1172a76a26045e90654c
PaulCiobanita <pciobanita@spartaglobal.com>
2019-01-08 13:12:36 +0000

    index paged done trhough erb

--- controller/sql_controller.rb
+++ controller/sql_controller.rb
@@ -1,5 +1,11 @@
 class SqlController < Sinatra::Base 
+     # sets root as the parent-directory of the current file
+  set :root, File.join(File.dirname(__FILE__), '..')
+
+  # sets the view directory correctly
+  set :views, Proc.new { File.join(root, "views") }
     get '/' do
-        "hello world"
+        erb :'posts/index'
     end
+
 end
