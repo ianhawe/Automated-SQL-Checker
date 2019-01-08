0119f4b5b7cb33cbe210b95060e0121af8ffca55
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2019-01-08 14:18:31 +0000

    Paths changed for review pages

--- controller/sql_controller.rb
+++ controller/sql_controller.rb
@@ -2,11 +2,13 @@ class SqlController < Sinatra::Base
 
     set :root, File.join(File.dirname(__FILE__), '..')
     set :views, Proc.new {File.join(root, "views") }
-    get '/student-review' do
+
+    get '/student/review' do
         erb :'student-review'
     end
 
-    get '/admin-student-review' do
+    get '/admin/student-review' do
         erb :'admin-student-review'
     end
+
 end
