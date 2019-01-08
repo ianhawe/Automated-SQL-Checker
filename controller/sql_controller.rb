55ab6af71c1781d91cd149ac60050ac8382e3e07
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2019-01-08 13:30:31 +0000

    Completed Admin student review page and student review page with working routes

--- controller/sql_controller.rb
+++ controller/sql_controller.rb
@@ -1,5 +1,12 @@
 class SqlController < Sinatra::Base 
-    get '/' do
-        "hello world"
+
+    set :root, File.join(File.dirname(__FILE__), '..')
+    set :views, Proc.new {File.join(root, "views") }
+    get '/student-review' do
+        erb :'student-review'
+    end
+
+    get '/admin-student-review' do
+        erb :'admin-student-review'
     end
 end
