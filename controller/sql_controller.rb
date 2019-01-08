24f6497ceb22b2126b8f337275f7902b6725c495
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2019-01-08 14:38:39 +0000

    Completed info page

--- controller/sql_controller.rb
+++ controller/sql_controller.rb
@@ -1,5 +1,11 @@
 class SqlController < Sinatra::Base 
-    get '/' do
-        "hello world"
+
+    # sets root as the parent-directory of the current file
+    set :root, File.join(File.dirname(__FILE__), '..')
+    # sets the view directory correctly
+    set :views, Proc.new { File.join(root, "views") }
+
+    get '/student/info-page' do
+        erb :'/info_page'
     end
 end
