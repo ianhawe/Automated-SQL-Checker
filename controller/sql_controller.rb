7c3bc8a232fa0c792253b48f7e06a39536dddbbf
Raiyan Shaheen <r.12345@hotmail.co.uk>
2019-01-08 14:31:03 +0000

    initialiased and routed for question 1 controller

--- controller/sql_controller.rb
+++ controller/sql_controller.rb
@@ -1,5 +1,21 @@
 class SqlController < Sinatra::Base 
+
+      # sets root as the parent-directory of the current file
+  set :root, File.join(File.dirname(__FILE__), '..')
+
+    # sets the view directory correctly
+  set :views, Proc.new { File.join(root, "views") }
+
     get '/' do
-        "hello world"
+        erb  :'posts/index'
     end
+    
+    get '/student/login' do
+        erb  :'posts/student_login'
+    end
+
+    get 'student/question/1' do
+        erb  :'posts/question_one_page'
+    end
+
 end
