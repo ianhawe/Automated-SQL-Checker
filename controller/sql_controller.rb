cea051a31f87a333c053828d1f503292edf33d7f
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2019-01-08 12:22:02 +0000

    set up the view of the page

--- controller/sql_controller.rb
+++ controller/sql_controller.rb
@@ -1,5 +1,10 @@
 class SqlController < Sinatra::Base 
+
+    set :root, File.join(File.dirname(__FILE__), '..')
+
+    set :views, Proc.new { File.join(root, "views") }
+
     get '/' do
-        "hello world"
+        erb :'question-one-page'
     end
 end
